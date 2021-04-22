6f73b24ead46dc61b43a922936c6f93a805b6544
jchuahtacc <jchuah@tacc.utexas.edu>
2021-04-22 13:48:01 -0500

    get_users integration test

--- src/jupyterhub_admin/jhub_api.py
+++ src/jupyterhub_admin/jhub_api.py
@@ -1,19 +1,25 @@
 from django.conf import settings
 import requests
-
+import logging
 
 def jupyterhub_request(method, endpoint, data=None, params=None):
     api_url = settings.JUPYTERHUB_API + endpoint
     headers = {
         'Authorization': 'token %s' % settings.JUPYTERHUB_TOKEN
     }
+    print(api_url)
+    print(headers)
     if method == 'POST':
-        return requests.post(api_url, data=data)
+        return requests.post(api_url, data=data, headers=headers)
     elif method == 'GET':
-        return requests.get(api_url, params=params)
+        return requests.get(api_url, params=params, headers=headers)
     elif method == 'DELETE':
-        return requests.get(api_url, params=params)
+        return requests.get(api_url, params=params, headers=headers)
 
 
 def get_version():
     return jupyterhub_request('GET', '/').json()
+
+
+def get_users():
+    return jupyterhub_request('GET', '/users').json()
--- src/jupyterhub_admin/jhub_api_integration_test.py
+++ src/jupyterhub_admin/jhub_api_integration_test.py
@@ -1,5 +1,9 @@
-from jupyterhub_admin.jhub_api import get_version
+from jupyterhub_admin.jhub_api import get_version, get_users
 
 
 def test_get_version():
     assert "version" in get_version()
+
+
+def test_get_users():
+    assert isinstance(get_users(), list)
