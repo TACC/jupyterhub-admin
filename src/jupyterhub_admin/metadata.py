dd5d5194e5fc8fba439a3027708af59a4c08083d
jchuahtacc <jchuah@tacc.utexas.edu>
2021-06-28 14:10:28 -0500

    group metadata utility functions

--- src/jupyterhub_admin/metadata.py
+++ src/jupyterhub_admin/metadata.py
@@ -7,18 +7,44 @@ def get_config_metadata_name():
     return f"config.{settings.TENANT}.{settings.INSTANCE}.jhub"
 
 
+def get_group_config_metadata_name(group):
+    return f"{group}.group.{get_config_metadata_name()}"
+
+
+def get_agave_client():
+    return Agave(api_server=settings.AGAVE_API, token=settings.AGAVE_SERVICE_TOKEN)
+
+
 def get_config_metadata():
-    ag = Agave(api_server=settings.AGAVE_API, token=settings.AGAVE_SERVICE_TOKEN)
-    metadata = ag.meta.listMetadata()
-    matching = list(filter(
-        lambda meta: meta['name'] == get_config_metadata_name(),
-        metadata
-    ))
-    return matching[0]
+    ag = get_agave_client()
+    query = {'name':get_config_metadata_name()}
+    metadata = ag.meta.listMetadata(q=json.dumps(query))
+    return metadata[0]
+
+
+def list_group_config_metadata():
+    ag = get_agave_client()
+    query = { 'name': {'$regex': f".*group.{get_config_metadata_name()}"}}
+    metadata = ag.meta.listMetadata(q=json.dumps(query))
+    return metadata
+
+
+def get_group_config_metadata(group):
+    ag = get_agave_client()
+    query = {'name': get_group_config_metadata_name(group)}
+    metadata = ag.meta.listMetadata(q=json.dumps(query))
+    return metadata[0]
+
+
+def write_group_config_metadata(group, value):
+    ag = get_agave_client()
+    meta = get_group_config_metadata(group)
+    meta['value'] = value
+    ag.meta.updateMetadata(body=meta, uuid=meta['uuid'])
 
 
 def write_config_metadata(value):
-    ag = Agave(api_server=settings.AGAVE_API, token=settings.AGAVE_SERVICE_TOKEN)
+    ag = get_agave_client()
     meta = get_config_metadata()
     meta['value'] = value
     ag.meta.updateMetadata(body=meta, uuid=meta['uuid'])
--- src/jupyterhub_admin/metadata_unit_test.py
+++ src/jupyterhub_admin/metadata_unit_test.py
@@ -3,13 +3,17 @@ from jupyterhub_admin.metadata import (
     get_config_metadata_name,
     get_config_metadata,
     write_config_metadata,
-    set_config
+    set_config,
+    get_group_config_metadata,
+    list_group_config_metadata,
+    write_group_config_metadata,
+    get_group_config_metadata_name
 )
 
 
 @pytest.fixture
 def mock_agave(mocker):
-    yield mocker.patch('jupyterhub_admin.metadata.Agave', autouse=True)
+    yield mocker.patch('jupyterhub_admin.metadata.get_agave_client', autouse=True)
 
 
 def test_get_config_metadata_name():
@@ -26,7 +30,7 @@ def test_get_config_metadata(mock_agave):
     assert get_config_metadata()['value'] == 'MOCK_VALUE'
 
 
-def test_get_write_config_metadata(mocker, mock_agave):
+def test_write_config_metadata(mocker, mock_agave):
     mocker.patch('jupyterhub_admin.metadata.get_config_metadata', return_value={
         'uuid': 'MOCK_UUID'  
     })
@@ -57,3 +61,45 @@ def test_set_config_new(mocker):
     mock_write = mocker.patch('jupyterhub_admin.metadata.write_config_metadata')
     set_config('key2', 'value2')
     mock_write.assert_called_with({ 'key1': 'value1', 'key2': 'value2' })
+
+
+def test_get_group_config_metadata_name():
+    assert get_group_config_metadata_name("MYGROUP") == "MYGROUP.group.config.JUPYTERHUB.PROD.jhub"
+
+
+def test_get_group_config_metadata(mock_agave):
+    mock_agave.return_value.meta.listMetadata.return_value = [
+        {
+            'name': 'MYGROUP.group.config.JUPYTERHUB.PROD.jhub',
+            'value': 'MOCK_VALUE'
+        }
+    ]
+    assert get_group_config_metadata("MYGROUP")['value'] == 'MOCK_VALUE'
+    mock_agave.return_value.meta.listMetadata.assert_called_with(
+        q='{"name": "MYGROUP.group.config.JUPYTERHUB.PROD.jhub"}'
+    )
+
+
+def test_list_group_config_metadata(mock_agave):
+    mock_agave.return_value.meta.listMetadata.return_value = [
+        {
+            'name': 'MYGROUP.group.config.JUPYTERHUB.PROD.jhub',
+            'value': 'MOCK_VALUE'
+        }
+    ]
+    assert list_group_config_metadata()[0]['value'] == 'MOCK_VALUE'
+    mock_agave.return_value.meta.listMetadata.assert_called_with(
+        q='{"name": {"$regex": ".*group.config.JUPYTERHUB.PROD.jhub"}}'
+    )
+
+
+def test_write_group_config_metadata(mocker, mock_agave):
+    mock_meta_get = mocker.patch('jupyterhub_admin.metadata.get_group_config_metadata', return_value={
+        'uuid': 'MOCK_UUID'  
+    })
+    write_group_config_metadata("MYGROUP", { 'key': 'value' })
+    mock_meta_get.assert_called_with("MYGROUP")
+    mock_agave.return_value.meta.updateMetadata.assert_called_with(
+        body={'uuid': 'MOCK_UUID', 'value': {'key': 'value'}},
+        uuid='MOCK_UUID'
+    )
